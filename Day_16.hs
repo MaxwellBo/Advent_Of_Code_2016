15149f6fa7845cbcd29ebfea27c2ca14d29e42f1
Max Bo <max@maxbo.me>
2016-12-21 20:14:07 +1000

    Add Day 16

---
+++ Day_16.hs
@@ -0,0 +1,25 @@
+import Data.Function ((&))
+
+main :: IO ()
+main = print . checksum $ fillDisk diskLength input
+    where 
+      diskLength = 272
+      input = "11100010111110100"
+
+process :: String -> String
+process a = a ++ "0" ++ b 
+  where
+    b = (fmap (\x -> if x == '1' then '0' else '1')) . reverse $ a 
+
+checksum :: String -> String
+checksum = (until (odd . length) pairsum) . pairsum
+
+pairsum :: String -> String
+pairsum (x:y:xs) 
+  | x == y    = '1' : pairsum xs
+  | otherwise = '0' : pairsum xs
+pairsum _ = []
+
+fillDisk :: Int -> String -> String
+fillDisk diskLength = (take diskLength) 
+                    . (until (\x -> length x >= diskLength) process)
