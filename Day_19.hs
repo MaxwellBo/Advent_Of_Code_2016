23e4298ec2f744af442a926bf22b3d4ea055bf80
Max Bo <max@maxbo.me>
2016-12-21 23:34:09 +1000

    Add Day 19

---
+++ Day_19.hs
@@ -0,0 +1,28 @@
+import Data.Sequence as S
+
+type Elfs = S.Seq Int
+
+main :: IO ()
+main = do
+  print partOne -- 1830117
+  print partTwo -- 1417887
+
+partOne :: Elfs
+partOne = until (\x -> S.length x == 1) (rotate . popNext) elfs
+
+partTwo :: Elfs
+partTwo = until (\x -> S.length x == 1) (rotate . popOpposite) elfs
+
+elfs :: Elfs
+elfs = S.fromList [1..3012210]
+
+popOpposite :: Elfs -> Elfs
+popOpposite elfs = S.deleteAt index elfs
+  where
+    index = S.length elfs `div` 2
+
+popNext :: Elfs -> Elfs
+popNext = S.deleteAt 1
+
+rotate :: Elfs -> Elfs
+rotate elfs = S.drop 1 elfs >< S.take 1 elfs
