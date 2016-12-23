32ae5a43a2065c13c399702aea3014477c93ed5d
Max Bo <max@maxbo.me>
2016-12-23 15:37:38 +1000

    Add more functions

--- Day_21.hs
+++ Day_21.hs
@@ -6,12 +6,11 @@ import Control.Applicative
 main :: IO ()
 main = putStrLn "Hello World"
 
-
 swapElemsAt :: Int -> Int -> [a] -> [a]
 swapElemsAt a b xs = left ++ [xs !! b] ++ middle ++ [xs !! a] ++ right
   where
-    left = take a xs;
-    middle = drop (succ a) (take b xs);
+    left = take a xs
+    middle = drop (succ a) (take b xs)
     right = drop (succ b) xs
 
 swapElems :: (Eq a) => a -> a -> [a] -> [a]
@@ -23,3 +22,19 @@ swapElems a b xs = swapElemsAt a' b' xs
 rotate :: Either () () -> Int -> [a] -> [a]
 rotate (Left _) n xs = drop n xs ++ take n xs
 rotate (Right _) n xs = rotate (Left ()) (length xs - n) xs
+
+rotateOnPos :: (Eq a) => a -> [a] -> [a]
+rotateOnPos x xs 
+  = if pos >= 4 then
+      rotate (Right ()) (pos + 2) xs
+    else
+      rotate (Right ()) (pos + 1) xs 
+  where
+    (Just pos) = elemIndex x xs
+
+reverseSpan :: Int -> Int -> [a] -> [a]
+reverseSpan a b xs = left ++ reverse middle ++ right
+  where
+    left = take a xs
+    middle = drop a (take (succ b) xs)
+    right = drop (succ b) xs
