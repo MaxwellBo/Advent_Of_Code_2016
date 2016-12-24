ab440c4fc9250dd31b50b99ce94cbe7e5ccf31d4
Max Bo <max@maxbo.me>
2016-12-24 12:28:18 +1000

    Finish move function

--- Day_21.hs
+++ Day_21.hs
@@ -1,17 +1,20 @@
 import Data.List
 import Data.Either
 import Control.Applicative
-
+import qualified Data.Sequence as S
+import Data.Foldable
 
 main :: IO ()
 main = putStrLn "Hello World"
 
+--swapElemsAt a b xs = left ++ [xs !! b] ++ middle ++ [xs !! a]
+--  where
+--    left = take a xs
+--    middle = drop (succ a) (take b xs)
+--    right = drop (succ b) xs
+
 swapElemsAt :: Int -> Int -> [a] -> [a]
-swapElemsAt a b xs = left ++ [xs !! b] ++ middle ++ [xs !! a] ++ right
-  where
-    left = take a xs
-    middle = drop (succ a) (take b xs)
-    right = drop (succ b) xs
+swapElemsAt a b = reverseSpan (a + 1) (b - 1) . reverseSpan a b
 
 swapElems :: (Eq a) => a -> a -> [a] -> [a]
 swapElems a b xs = swapElemsAt a' b' xs
@@ -38,3 +41,9 @@ reverseSpan a b xs = left ++ reverse middle ++ right
     left = take a xs
     middle = drop a (take (succ b) xs)
     right = drop (succ b) xs
+
+-- Sue me
+move :: Int -> Int -> [a] -> [a]
+move a b xs = toList . (S.insertAt b elem) . (S.deleteAt a) . S.fromList $ xs
+  where 
+    elem = S.index (S.fromList xs) a
