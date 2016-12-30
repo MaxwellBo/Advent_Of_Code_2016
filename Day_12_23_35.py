3852cd904b59b10911856c8b800a2fe573f8770e
Max Bo <max@maxbo.me>
2016-12-30 21:33:02 +1000

    Merge problems

--- Day_12_23_35.py
+++ Day_12_23_35.py
@@ -1,8 +1,5 @@
 # http://adventofcode.com/2016/day/12
 
-# Day 12 = v0
-# Day 23 = v1
-
 def main(day, part):
     with open("inputs/Day_{}_input.txt".format(day)) as fp:
 
@@ -11,9 +8,9 @@ def main(day, part):
 
         # Build computer
         pc = 0
-        registers = { "a": 0
+        registers = { "a": (7 if part == 1 else 12) if day == 23 else 0 
                     , "b": 0
-                    , "c": 0 if day == 12 and part == 1 else 1
+                    , "c": 1 if day == 12 and part == 2 else 0
                     , "d": 0
                     }
 
@@ -21,23 +18,19 @@ def main(day, part):
         while pc < len(program):
             ins = program[pc]
 
-            # Takes one l-value, and one r-value
             if ins[0] == "cpy":
-                val = int(ins[1]) if ins[1].isdigit() else registers[ins[1]]
+                val = int(ins[1]) if ins[1].lstrip('-').isdigit() else registers[ins[1]]
                 try:
                     registers[ins[2]] = val
                 except:
                     pass
 
-            # Takes one l-value
             elif ins[0] == "inc":
                 registers[ins[1]] += 1
 
-            # Takes one l-value
             elif ins[0] == "dec":
                 registers[ins[1]] -= 1
 
-            # Takes two l/r-values
             elif ins[0] == "jnz":
                 test = int(ins[1]) if ins[1].isdigit() else registers[ins[1]]
                 jump = int(ins[2]) if ins[2].lstrip('-').isdigit() else registers[ins[2]]
@@ -48,16 +41,17 @@ def main(day, part):
             elif ins[0] == "tgl":
                 i = pc + registers[ins[1]]
                 
-                if len(program[i]) == 2:
-                    if program[i][0] == "inc":
-                        program[i][0] = "dec"
+                if i < len(program): 
+                    if len(program[i]) == 2:
+                        if program[i][0] == "inc":
+                            program[i][0] = "dec"
+                        else:
+                            program[i][0] = "inc"
                     else:
-                        program[i][0] = "inc"
-                else:
-                    if program[i][0] == "jnz":
-                        program[i][0] = "cpy"
-                    else:
-                        program[i][0] = "jnz"
+                        if program[i][0] == "jnz":
+                            program[i][0] = "cpy"
+                        else:
+                            program[i][0] = "jnz"
 
 
             pc += 1
@@ -68,4 +62,4 @@ def main(day, part):
 if __name__ == '__main__':
     main(12, 1) # a: 318020
     main(12, 2) # a: 9227674
-    main(23, 1)
+    main(23, 1) # a: 11683
