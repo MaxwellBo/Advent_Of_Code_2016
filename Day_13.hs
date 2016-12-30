b0ad764974ec8bbca68e7d44bf6fc737e9fc577a
Max Bo <max@maxbo.me>
2016-12-30 13:29:06 +1000

    Get 13 fast boi

--- Day_13.hs
+++ Day_13.hs
@@ -1,9 +1,7 @@
 -- http://adventofcode.com/2016/day/13
 
-import Numeric
-import Data.Char
-import Control.Monad.Writer
-
+import Data.Bits
+import Data.List
 
 type Position = (Int, Int)
 
@@ -15,15 +13,9 @@ main = do
   print partOne
 
 partOne :: Int
-partOne = getSum . execWriter . until (isAtDestination) (>>= moveCount) $ start
+partOne = length . takeWhile (\x -> not $ ((31, 39) `elem` x)) $ search 
   where
-    start = return [(1, 1)]
-    isAtDestination x = (31, 39) `elem` (fst . runWriter $ x)
-
-moveCount :: [Position] -> Writer (Sum Int) [Position]
-moveCount xs = do
-  tell (Sum 1)
-  return (xs >>= move)
+    search = iterate (nub . concatMap move) [(1, 1)]
 
 move :: Position -> [Position]
 move (x, y) = filter (isOpen) $ [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
@@ -31,7 +23,4 @@ move (x, y) = filter (isOpen) $ [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
 isOpen :: Position -> Bool
 isOpen (x,y)
   | x < 0 || y < 0 = False
-  | otherwise = even . length . filter (=='1') . binary $ go
-  where
-    go = (x*x + 3*x + 2*x*y + y + y*y) + seed
-    binary x = showIntAtBase 2 intToDigit x ""
+  | otherwise = even . popCount $ (x*x + 3*x + 2*x*y + y + y*y) + seed
