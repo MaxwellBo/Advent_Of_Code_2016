6d29fc2dfbdc16429ee4684ee270b33e8f98c844
Max Bo <max@maxbo.me>
2017-01-08 22:12:20 +1000

    Add Day 17

---
+++ Day_17.hs
@@ -0,0 +1,64 @@
+-- http://adventofcode.com/2016/day/17
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.List
+import Data.Ord
+import Data.Digest.Pure.MD5 (md5)
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Monoid
+
+type Path = C.ByteString
+
+type Position = ((Int, Int), Path)
+
+seed :: Path
+seed = "qljzarfv"
+
+main :: IO ()
+main = do 
+  print partOne -- DRLRDDURDR
+
+partOne :: Path
+partOne = C.drop (C.length seed)
+        . head
+        . fmap snd
+        . filter (isDoor)
+        . until (any isDoor) (neighbors =<<)
+        $ [((0, 0), seed)]
+  where
+    isDoor = (==) (3, 3) . fst
+
+partTwo :: Path
+partTwo = C.drop (C.length seed)
+        . head
+        . fmap snd
+        . filter (isDoor)
+        . until (all isDoor) (neighbors =<<)
+        $ [((0, 0), seed)]
+  where
+    isDoor = (==) (3, 3) . fst
+
+bfs :: [Position] -> [[Position]]
+bfs = iterate (nub . (neighbors =<<)) 
+
+neighbors :: Position -> [Position]
+neighbors pos@((3, 3), p) = [pos]
+neighbors ((x, y), p) = filterAccessible p $  [ ((x, y - 1), p <> "U")
+                                              , ((x, y + 1), p <> "D")
+                                              , ((x - 1, y), p <> "L")
+                                              , ((x + 1, y), p <> "R")
+                                              ]
+
+isInBounds :: Position -> Bool
+isInBounds ((x, y), _)
+  | x < 0 || y < 0 || x > 3 || y > 3 = False
+  | otherwise = True
+
+filterAccessible :: Path -> [Position] -> [Position]
+filterAccessible p xs = (filter isInBounds) 
+                      . (fmap snd) 
+                      . (filter isOpen) $ (zipWith (,) (hash p) xs) 
+  where
+    isOpen (door, _) = door `elem` ['b'..'f']
+    hash = show . md5
