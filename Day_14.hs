613048be30ec370c64c7509e6772695b25c55251
Max Bo <max@maxbo.me>
2017-01-12 00:36:35 +1000

    Put the Haskell version of 14 on hold

--- Day_14.hs
+++ Day_14.hs
@@ -14,20 +14,37 @@ type Index = Int
 type Pair = (Index, Hash)
 
 hashes :: [Pair]
-hashes = fmap (\x -> (x, show . md5 $ ("abc" <> (C.pack . show $ x)))) [0..]
+hashes = fmap (x, \x -> (show . md5 $ ("abc" <> (C.pack . show $ x)))) [0..]
 
 main :: IO ()
 main = do 
   print hashes
 
-subsequencesOfLength :: Int -> String -> [String]
-subsequencesOfLength n = filter ((n==) . length) . subsequences
+xtuples :: Int -> String -> [String]
+xtuples n = filter isHomogeneous . filter ((n==) . length) . subsequences
 
--- quintupleFollows :: Char -> Maybe ()
--- quintupleFollows = undefined
+isHomogeneous :: String -> Bool
+isHomogeneous xs = and $ zipWith (==) xs (tail xs)
 
--- containsTriple :: Hash -> Maybe Char
--- containsTriple = listToMaybe <=< listToMaybe . subsequencesOfLength 3
+quintupleFollows :: Int -> Char -> Bool
+quintupleFollows index char = (char `elem`)
+                            . nub
+                            . mconcat
+                            . fmap (quintupleChars)
+                            . take 1000
+                            . drop index 
+                            $ hashes
 
--- isKey :: [Pair] -> Maybe ()
--- isKey p@(index, hash) = quintupleFollows p <=< containsTriple hash
+quintupleChars :: Hash -> [Char]
+quintupleChars = nub 
+               . fmap head 
+               . xtuples 5
+                            
+tripleChar :: Hash -> Char
+tripleChar = fromMaybe '?'
+           . listToMaybe
+           . fmap head
+           . xtuples 3
+
+isKey :: [Pair] -> Maybe ()
+isKey (index, hash) = quintupleFollows index . tripleChar $ hash
