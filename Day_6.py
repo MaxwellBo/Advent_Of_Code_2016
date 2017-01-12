a42a471f1aca384cd02504e70b6d14a1c7dc23c3
Max Bo <max@maxbo.me>
2017-01-13 00:59:51 +1000

    Kill whitespace

--- Day_6.py
+++ Day_6.py
@@ -3,14 +3,10 @@
 from collections import Counter
 
 def main(part):
-
     with open("inputs/Day_6_input.txt") as fp:
         rotated_message = [  "".join(char) for char in zip(*fp.read().split()[::-1]) ]
 
-        index = 0 if part == 1 else -1
-
-        # for least common, change the following index to -1
-        #                                                       v
+        index = 0 if part == 1 else -1 # -1 getting the least common
         error_corrected = "".join(Counter(line).most_common()[index][0] for line in rotated_message)
 
         print(f"Part {part}:", error_corrected)
