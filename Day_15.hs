5aa5e0917c1709543cf4373b1b3a46b1839c1f60
Max Bo <max@maxbo.me>
2017-01-14 10:02:36 +1000

    Progress on Day 15

--- Day_15.hs
+++ Day_15.hs
@@ -3,6 +3,7 @@
 module Day_15 where
 
 import Data.List.Split
+import Data.Bifunctor
 
 type Position = Int
 type Positions = Int
@@ -15,7 +16,18 @@ main = do
 
 partOne = (fmap parseDisc) . lines
 
+compensate :: [Disc] -> [Disc]
+compensate = zipWith (\y (a, b)-> (a + y, b)) [1..]
+
+wait :: [Disc] -> [Disc]
+wait = fmap.first $ (+1)
+
+allClear :: [Disc] -> Bool
+allClear = all isOpen
+  where
+    isOpen disc = (fst disc `mod` snd disc) == 0
+
 parseDisc :: String -> Disc
-parseDisc xs = (read $ tok !! 3, read $ tok !! 11)
+parseDisc xs = (read $ tok !! 11, read $ tok !! 3)
   where
     tok = splitOn " " . init $ xs
